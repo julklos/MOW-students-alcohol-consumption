8acd82715ec19cd391751c213c9619476101944a
Julia <julia.klos159@gmail.com>
2020-05-28 14:27:35 +0200

    testy

--- src/forest-test.R
+++ src/forest-test.R
@@ -5,6 +5,7 @@ library(randomForest)
 library(tidyverse)
 library(tidyr)
 library(caret)
+library(rfUtilities)
 
 students<- read.csv("../data/students.csv",header=TRUE)
 students$X <- NULL
@@ -16,3 +17,38 @@ students$Dalc = as.factor(students$Dalc)
 sample <- sample.int(n = nrow(students), size = floor(.8*nrow(students)), replace = F)
 train <- students[sample, ]
 test  <- students[-sample, ]
+
+mtryDf<- data.frame(mtry=integer(),
+                 err=double(),
+                 errTest=double())
+maxNodesDf<- data.frame(maxNodes=integer(),
+                    err=double(),
+                    errTest=double())
+numTreesDf <- data.frame(numtrees=integer(),
+                               err=double(),
+                         errTest=double())
+
+mtryAr = c(1:30)
+numTreesAr = c(1, 10, 30, 50, 100, 300, 500, 1000, 3000)
+maxNodseAr = c(1, 5, 10, 25, 30, 60, 100)
+iters = 20
+constmMtry = 9
+constMaxNodes = 60
+constNtrees = 300
+for (att in mtryAr){
+  ## opcja jeden, ewentualnie CV/ OBB- to bardziej do rysunku prowadzi
+  for(i in 1:iters){
+    sample <- sample.int(n = nrow(students), size = floor(.8*nrow(students)), replace = F)
+    train <- students[sample, ]
+    test  <- students[-sample, ]
+
+    model <- randomForest(Dalc ~ . , data = train, mtry = att, ntree =constNtrees, replace = TRUE, maxnodes = constMaxNodes ) 
+    
+    newValues <- data.frame(att, err(train$Dalc, predict(model, train)), err(test$Dalc, predict(model, test)) )      
+    names(newValues) <- c("mtry", "err", "errTest")  
+    mtryDf <- rbind(mtryDf, newValues)  
+    
+  }
+}
+boxplot(err~mtry, data=mtryDf,xlab="mytry", ylab="błąd")
+boxplot(errTest~mtry, data=mtryDf,xlab="mytry", ylab="błąd")

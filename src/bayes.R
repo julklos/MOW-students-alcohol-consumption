ff88ee487fbdcf5b7d6f3ba682aa7ae2dbb59662
Julia <julia.klos159@gmail.com>
2020-06-03 15:59:38 +0200

    bayes

--- src/bayes.R
+++ src/bayes.R
@@ -98,93 +98,185 @@ predc <- function(...) { predict(..., type="c") }
 
 # 10-krotna walidacja krzyżowa na zbiorze trenującym
 # przy domyślnych parametrach
+          
 cv100_Dalc <- tune(naiveBayes, Dalc ~ ., data=train,
-                predict.func=predc, metric = "Accuracy",
-                tunecontrol=tune.control(sampling="cross", cross=10))
-# sprawdźmy błąd uzyskany dla walidacji krzyżowej 0.3364659
+                   predict.func=predc, metric = "Accuracy",
+                   tunecontrol=tune.control(sampling="cross", cross=10))
+# sprawdźmy błąd uzyskany dla walidacji krzyżowej  0.334688
 cv100_Dalc
-# końcowy model zbudowanego na całości przekazanych danych
+
+cv100_Dalc$best.model
+bestModel_Dalc <- confusionMatrix( predict(cv100_Dalc$best.model, test),test_labels, mode="everything")
+bestModel_Dalc
+# Reference
+# Prediction  1  2  3  4  5
+# 1 74 13  2  2  0
+# 2 12  7  2  1  1
+# 3  3  4  2  2  2
+# 4  1  0  0  0  1
+# 5  1  1  0  2  0
+# 
+# Overall Statistics
+# 
+# Accuracy : 0.6241         
+# 95% CI : (0.536, 0.7065)
+# No Information Rate : 0.6842         
+# P-Value [Acc > NIR] : 0.9419         
+# 
+# Kappa : 0.2378         
+# 
+# Mcnemar's Test P-Value : 0.6704         
+# 
+# Statistics by Class:
+# 
+#                      Class: 1 Class: 2 Class: 3 Class: 4 Class: 5
+# Sensitivity            0.8132  0.28000  0.33333  0.00000  0.00000
+# Specificity            0.5952  0.85185  0.91339  0.98413  0.96899
+# Pos Pred Value         0.8132  0.30435  0.15385  0.00000  0.00000
+# Neg Pred Value         0.5952  0.83636  0.96667  0.94656  0.96899
+# Precision              0.8132  0.30435  0.15385  0.00000  0.00000
+# Recall                 0.8132  0.28000  0.33333  0.00000  0.00000
+# F1                     0.8132  0.29167  0.21053      NaN      NaN
+# Prevalence             0.6842  0.18797  0.04511  0.05263  0.03008
+# Detection Rate         0.5564  0.05263  0.01504  0.00000  0.00000
+# Detection Prevalence   0.6842  0.17293  0.09774  0.01504  0.03008
+# Balanced Accuracy      0.7042  0.56593  0.62336  0.49206  0.48450
+
+# zobaczmy jak błąd na zbiorze testowym ma się do błędu z walidacji krzyżowej
+err(test$Dalc, predict(cv100_Dalc$best.model, test, type="c")) #0.3759398
+
+## laplace
+cv100_Dalc_l <- tune(naiveBayes, Dalc ~ ., data=train,
+                   predict.func=predc, metric = "Accuracy", laplace = 100,
+                   tunecontrol=tune.control(sampling="cross", cross=10))
+# sprawdźmy błąd uzyskany dla walidacji krzyżowej 
+cv100_Dalc_l # 0.3136067
+
+cv100_Dalc_l$best.model
+bestModel_Dalc_l <- confusionMatrix( predict(cv100_Dalc$best.model, test),test_labels, mode="everything")
+bestModel_Dalc_l
+# 
 # Confusion Matrix and Statistics
 # 
 # Reference
 # Prediction  1  2  3  4  5
-# 1 79 15  3  2  0
-# 2  9  5  3  1  0
-# 3  3  2  1  2  2
-# 4  0  1  0  0  1
-# 5  1  2  0  1  0
+# 1 74 13  2  2  0
+# 2 12  7  2  1  1
+# 3  3  4  2  2  2
+# 4  1  0  0  0  1
+# 5  1  1  0  2  0
 # 
 # Overall Statistics
 # 
-# Accuracy : 0.6391          
-# 95% CI : (0.5513, 0.7205)
-# No Information Rate : 0.6917          
-# P-Value [Acc > NIR] : 0.9190          
+# Accuracy : 0.6241         
+# 95% CI : (0.536, 0.7065)
+# No Information Rate : 0.6842         
+# P-Value [Acc > NIR] : 0.9419         
 # 
-# Kappa : 0.2057          
+# Kappa : 0.2378         
 # 
-# Mcnemar's Test P-Value : 0.3814          
+# Mcnemar's Test P-Value : 0.6704         
 # 
 # Statistics by Class:
 # 
 #                      Class: 1 Class: 2 Class: 3 Class: 4 Class: 5
-# Sensitivity            0.8587  0.20000 0.142857  0.00000  0.00000
-# Specificity            0.5122  0.87963 0.928571  0.98425  0.96923
-# Pos Pred Value         0.7980  0.27778 0.100000  0.00000  0.00000
-# Neg Pred Value         0.6176  0.82609 0.951220  0.95420  0.97674
-# Precision              0.7980  0.27778 0.100000  0.00000  0.00000
-# Recall                 0.8587  0.20000 0.142857  0.00000  0.00000
-# F1                     0.8272  0.23256 0.117647      NaN      NaN
-# Prevalence             0.6917  0.18797 0.052632  0.04511  0.02256
-# Detection Rate         0.5940  0.03759 0.007519  0.00000  0.00000
-# Detection Prevalence   0.7444  0.13534 0.075188  0.01504  0.03008
-# Balanced Accuracy      0.6854  0.53981 0.535714  0.49213  0.48462
-cv100_Dalc$best.model
-bestModel_Dalc <- confusionMatrix( predict(cv100_Dalc$best.model, test),test_labels, mode="everything")
-bestModel_Dalc
+# Sensitivity            0.8132  0.28000  0.33333  0.00000  0.00000
+# Specificity            0.5952  0.85185  0.91339  0.98413  0.96899
+# Pos Pred Value         0.8132  0.30435  0.15385  0.00000  0.00000
+# Neg Pred Value         0.5952  0.83636  0.96667  0.94656  0.96899
+# Precision              0.8132  0.30435  0.15385  0.00000  0.00000
+# Recall                 0.8132  0.28000  0.33333  0.00000  0.00000
+# F1                     0.8132  0.29167  0.21053      NaN      NaN
+# Prevalence             0.6842  0.18797  0.04511  0.05263  0.03008
+# Detection Rate         0.5564  0.05263  0.01504  0.00000  0.00000
+# Detection Prevalence   0.6842  0.17293  0.09774  0.01504  0.03008
+# Balanced Accuracy      0.7042  0.56593  0.62336  0.49206  0.48450
+# zobaczmy jak błąd na zbiorze testowym ma się do błędu z walidacji krzyżowej
+err(test$Dalc, predict(cv100_Dalc_l$best.model, test, type="c")) #0.3458647
 
-# zobaczmy jak błąd na zbiorze testowym ma się do błędu z walidacji krzyżowej 0.3609023
-err(test$Dalc, predict(cv100_Dalc$best.model, test, type="c"))
-
-
+##weekend
 cv100_Walc <- tune(naiveBayes, Walc ~ ., data = train,
                    predict.func = predc, tunecontrol = tune.control(sampling = "cross", cross = 10))
 cv100_Walc$best.model
-cv100_Walc #0.5822206
+cv100_Walc  #0.5654572
+
+bestModel_Walc <- confusionMatrix(predict(cv100_Walc$best.model, test, type= "c"), test_labels_Wlc)
+bestModel_Walc
+# Confusion Matrix and Statistics
+# 
 # Reference
 # Prediction  1  2  3  4  5
-# 1 26 11 10  5  0
-# 2 25  3  2  3  0
-# 3  4  7  2  9  0
-# 4  0  4  4  7  5
-# 5  0  0  0  2  4
+# 1 23 14  5  3  2
+# 2 15  5  7  6  0
+# 3  7  1  4  4  2
+# 4  3  3  8  6  4
+# 5  1  1  4  2  3
 # 
 # Overall Statistics
 # 
-# Accuracy : 0.3158        
-# 95% CI : (0.238, 0.402)
-# No Information Rate : 0.4135        
-# P-Value [Acc > NIR] : 0.992         
+# Accuracy : 0.3083          
+# 95% CI : (0.2311, 0.3942)
+# No Information Rate : 0.3684          
+# P-Value [Acc > NIR] : 0.9383          
 # 
-# Kappa : 0.0714        
+# Kappa : 0.0913          
 # 
-# Mcnemar's Test P-Value : NA            
+# Mcnemar's Test P-Value : 0.4522          
 # 
 # Statistics by Class:
 # 
 #                      Class: 1 Class: 2 Class: 3 Class: 4 Class: 5
-# Sensitivity            0.4727  0.12000  0.11111  0.26923  0.44444
-# Specificity            0.6667  0.72222  0.82609  0.87850  0.98387
-# Pos Pred Value         0.5000  0.09091  0.09091  0.35000  0.66667
-# Neg Pred Value         0.6420  0.78000  0.85586  0.83186  0.96063
-# Prevalence             0.4135  0.18797  0.13534  0.19549  0.06767
-# Detection Rate         0.1955  0.02256  0.01504  0.05263  0.03008
-# Detection Prevalence   0.3910  0.24812  0.16541  0.15038  0.04511
-# Balanced Accuracy      0.5697  0.42111  0.46860  0.57387  0.71416
-bestModel_Walc <- confusionMatrix(predict(cv100_Walc$best.model, test, type= "c"), test_labels_Wlc)
-bestModel_Walc
-err(test$Walc, predict(cv100_Walc$best.model, test, type="c")) #0.6842105
+# Sensitivity            0.4694  0.20833  0.14286  0.28571  0.27273
+# Specificity            0.7143  0.74312  0.86667  0.83929  0.93443
+# Pos Pred Value         0.4894  0.15152  0.22222  0.25000  0.27273
+# Neg Pred Value         0.6977  0.81000  0.79130  0.86239  0.93443
+# Prevalence             0.3684  0.18045  0.21053  0.15789  0.08271
+# Detection Rate         0.1729  0.03759  0.03008  0.04511  0.02256
+# Detection Prevalence   0.3534  0.24812  0.13534  0.18045  0.08271
+# Balanced Accuracy      0.5918  0.47573  0.50476  0.56250  0.60358
+err(test$Walc, predict(cv100_Walc$best.model, test, type="c")) #0.6917293
 
+## weekend laplace
+cv100_Walc_l <- tune(naiveBayes, Walc ~ ., data = train,
+                   predict.func = predc, tunecontrol = tune.control(sampling = "cross", cross = 10))
+cv100_Walc_l$best.model
+cv100_Walc_l #0.580479
+
+bestModel_Walc_l <- confusionMatrix(predict(cv100_Walc_l$best.model, test, type= "c"), test_labels_Wlc)
+bestModel_Walc_l
+# Confusion Matrix and Statistics
+# 
+# Reference
+# Prediction  1  2  3  4  5
+# 1 23 14  5  3  2
+# 2 15  5  7  6  0
+# 3  7  1  4  4  2
+# 4  3  3  8  6  4
+# 5  1  1  4  2  3
+# 
+# Overall Statistics
+# 
+# Accuracy : 0.3083          
+# 95% CI : (0.2311, 0.3942)
+# No Information Rate : 0.3684          
+# P-Value [Acc > NIR] : 0.9383          
+# 
+# Kappa : 0.0913          
+# 
+# Mcnemar's Test P-Value : 0.4522          
+# 
+# Statistics by Class:
+# 
+#                      Class: 1 Class: 2 Class: 3 Class: 4 Class: 5
+# Sensitivity            0.4694  0.20833  0.14286  0.28571  0.27273
+# Specificity            0.7143  0.74312  0.86667  0.83929  0.93443
+# Pos Pred Value         0.4894  0.15152  0.22222  0.25000  0.27273
+# Neg Pred Value         0.6977  0.81000  0.79130  0.86239  0.93443
+# Prevalence             0.3684  0.18045  0.21053  0.15789  0.08271
+# Detection Rate         0.1729  0.03759  0.03008  0.04511  0.02256
+# Detection Prevalence   0.3534  0.24812  0.13534  0.18045  0.08271
+# Balanced Accuracy      0.5918  0.47573  0.50476  0.56250  0.60358
+err(test$Walc, predict(cv100_Walc_l$best.model, test, type="c")) #0.6917293
 
 rm(list=ls())
 dev.off()
